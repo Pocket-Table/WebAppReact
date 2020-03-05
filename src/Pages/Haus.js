1c8ae6ba12feae817c82cd7c4e05ada035d6e9d2
Iosif Radu <raduiosif15@yahoo.com>
2020-03-05 21:58:14 +0200

    haus2

--- src/Pages/Haus.js
+++ src/Pages/Haus.js
@@ -5,7 +5,7 @@ class Haus extends React.Component {
             <React.Fragment>
                 <Header />
                 <Navbar tab={1}/>
-                <h1 id="title">Haus page</h1>
+                <h1 id="title">Haus haus haus haus page</h1>
             </React.Fragment>
         );
     }
